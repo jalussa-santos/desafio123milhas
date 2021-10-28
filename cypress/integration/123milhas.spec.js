a457c41cbca740989648644800efe43e3b08424c
Jalussa dos Santos <jalussa.santos@gmail.com>
2021-10-27 22:42:25 -0300

    add teste e pipeline

---
+++ cypress/integration/123milhas.spec.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+describe('123milhas', () => {
+  beforeEach(() => {
+    cy.visit('')
+  })
+
+  it('Buscar hotel', () => {
+    cy.get('#hoteis > .nav-link > .description-tab').click()
+    cy.get('#txtHotelLocation').type('Aleg')
+  })
+})
